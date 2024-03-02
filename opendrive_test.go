c4b714b9ab1825cde6d7e4cbcffc5ca357abaaaa
Christian Birchler <birchler.chr@gmail.com>
2024-03-02 22:13:54 +0100

    test: test correct coords

--- opendrive_test.go
+++ opendrive_test.go
@@ -28,6 +28,18 @@ func TestToPointsShouldReturnTheRightNumberOfPoints(t *testing.T) {
 	}
 }
 
+func TestToPointsShouldReturnCorrectCoordinatPoints(t *testing.T) {
+	od := getExampleOpenDrive(t)
+	geos := od.Road.PlanView.Geometries
+	if geos[0].X > 64.8366 || geos[0].X < 64.836 {
+		t.Error("Wrong x coordinate for the first point:", geos[0].X)
+	}
+
+	if geos[0].Y > 10.1 || geos[0].Y < 10.0 {
+		t.Error("Wrong x coordinate for the first point:", geos[0].X)
+	}
+}
+
 const ExampleXodr = `<OpenDRIVE>
 <header name="mt/set_7/frenetic/odr/test-15">
 <sdc_test_info test_id="mt/set_7/frenetic/odr/test-15" test_outcome="FAIL" predicted_test_outcome="null" test_duration="8.018114805221558" is_valid="True" />
