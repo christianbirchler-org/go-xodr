7d1b7e50bb2b8123033347d35c2a1add5e573010
Christian Birchler <birchler.chr@gmail.com>
2024-07-12 14:32:44 +0200

    feat: define element interface

--- elements.gen.go
+++ elements.gen.go
@@ -2,332 +2,1217 @@
 
 package xodr
 
+type Element interface {
+	Parent() Element
+	Children() []Element
+}
+
 type OpenDRIVE struct {
+}
+
+func (e *OpenDRIVE) Parent() []Element {
+	return nil
+}
+
+func (e *OpenDRIVE) Children() []Element {
+	return nil
 }
 
 type Header struct {
 }
 
+func (e *Header) Parent() []Element {
+	return nil
+}
+
+func (e *Header) Children() []Element {
+	return nil
+}
+
 type GeoReference struct {
+}
+
+func (e *GeoReference) Parent() []Element {
+	return nil
+}
+
+func (e *GeoReference) Children() []Element {
+	return nil
 }
 
 type Offset struct {
 }
 
+func (e *Offset) Parent() []Element {
+	return nil
+}
+
+func (e *Offset) Children() []Element {
+	return nil
+}
+
 type License struct {
+}
+
+func (e *License) Parent() []Element {
+	return nil
+}
+
+func (e *License) Children() []Element {
+	return nil
 }
 
 type DefaultRegulations struct {
 }
 
+func (e *DefaultRegulations) Parent() []Element {
+	return nil
+}
+
+func (e *DefaultRegulations) Children() []Element {
+	return nil
+}
+
 type RoadRegulations struct {
+}
+
+func (e *RoadRegulations) Parent() []Element {
+	return nil
+}
+
+func (e *RoadRegulations) Children() []Element {
+	return nil
 }
 
 type SignalRegulations struct {
 }
 
+func (e *SignalRegulations) Parent() []Element {
+	return nil
+}
+
+func (e *SignalRegulations) Children() []Element {
+	return nil
+}
+
 type Road struct {
+}
+
+func (e *Road) Parent() []Element {
+	return nil
+}
+
+func (e *Road) Children() []Element {
+	return nil
 }
 
 type Link struct {
 }
 
+func (e *Link) Parent() []Element {
+	return nil
+}
+
+func (e *Link) Children() []Element {
+	return nil
+}
+
 type Predecessor struct {
+}
+
+func (e *Predecessor) Parent() []Element {
+	return nil
+}
+
+func (e *Predecessor) Children() []Element {
+	return nil
 }
 
 type Successor struct {
 }
 
+func (e *Successor) Parent() []Element {
+	return nil
+}
+
+func (e *Successor) Children() []Element {
+	return nil
+}
+
 type Type struct {
+}
+
+func (e *Type) Parent() []Element {
+	return nil
+}
+
+func (e *Type) Children() []Element {
+	return nil
 }
 
 type Speed struct {
 }
 
+func (e *Speed) Parent() []Element {
+	return nil
+}
+
+func (e *Speed) Children() []Element {
+	return nil
+}
+
 type PlanView struct {
+}
+
+func (e *PlanView) Parent() []Element {
+	return nil
+}
+
+func (e *PlanView) Children() []Element {
+	return nil
 }
 
 type Geometry struct {
 }
 
+func (e *Geometry) Parent() []Element {
+	return nil
+}
+
+func (e *Geometry) Children() []Element {
+	return nil
+}
+
 type Line struct {
+}
+
+func (e *Line) Parent() []Element {
+	return nil
+}
+
+func (e *Line) Children() []Element {
+	return nil
 }
 
 type Spiral struct {
 }
 
+func (e *Spiral) Parent() []Element {
+	return nil
+}
+
+func (e *Spiral) Children() []Element {
+	return nil
+}
+
 type Arc struct {
+}
+
+func (e *Arc) Parent() []Element {
+	return nil
+}
+
+func (e *Arc) Children() []Element {
+	return nil
 }
 
 type Poly3 struct {
 }
 
+func (e *Poly3) Parent() []Element {
+	return nil
+}
+
+func (e *Poly3) Children() []Element {
+	return nil
+}
+
 type ParamPoly3 struct {
+}
+
+func (e *ParamPoly3) Parent() []Element {
+	return nil
+}
+
+func (e *ParamPoly3) Children() []Element {
+	return nil
 }
 
 type ElevationProfile struct {
 }
 
+func (e *ElevationProfile) Parent() []Element {
+	return nil
+}
+
+func (e *ElevationProfile) Children() []Element {
+	return nil
+}
+
 type Elevation struct {
+}
+
+func (e *Elevation) Parent() []Element {
+	return nil
+}
+
+func (e *Elevation) Children() []Element {
+	return nil
 }
 
 type LateralProfile struct {
 }
 
+func (e *LateralProfile) Parent() []Element {
+	return nil
+}
+
+func (e *LateralProfile) Children() []Element {
+	return nil
+}
+
 type Superelevation struct {
+}
+
+func (e *Superelevation) Parent() []Element {
+	return nil
+}
+
+func (e *Superelevation) Children() []Element {
+	return nil
 }
 
 type Shape struct {
 }
 
+func (e *Shape) Parent() []Element {
+	return nil
+}
+
+func (e *Shape) Children() []Element {
+	return nil
+}
+
 type CrossSectionSurface struct {
+}
+
+func (e *CrossSectionSurface) Parent() []Element {
+	return nil
+}
+
+func (e *CrossSectionSurface) Children() []Element {
+	return nil
 }
 
 type TOffset struct {
 }
 
+func (e *TOffset) Parent() []Element {
+	return nil
+}
+
+func (e *TOffset) Children() []Element {
+	return nil
+}
+
 type Coefficients struct {
+}
+
+func (e *Coefficients) Parent() []Element {
+	return nil
+}
+
+func (e *Coefficients) Children() []Element {
+	return nil
 }
 
 type SurfaceStrips struct {
 }
 
+func (e *SurfaceStrips) Parent() []Element {
+	return nil
+}
+
+func (e *SurfaceStrips) Children() []Element {
+	return nil
+}
+
 type Strip struct {
+}
+
+func (e *Strip) Parent() []Element {
+	return nil
+}
+
+func (e *Strip) Children() []Element {
+	return nil
 }
 
 type Width struct {
 }
 
+func (e *Width) Parent() []Element {
+	return nil
+}
+
+func (e *Width) Children() []Element {
+	return nil
+}
+
 type Constant struct {
+}
+
+func (e *Constant) Parent() []Element {
+	return nil
+}
+
+func (e *Constant) Children() []Element {
+	return nil
 }
 
 type Linear struct {
 }
 
+func (e *Linear) Parent() []Element {
+	return nil
+}
+
+func (e *Linear) Children() []Element {
+	return nil
+}
+
 type Quadratic struct {
+}
+
+func (e *Quadratic) Parent() []Element {
+	return nil
+}
+
+func (e *Quadratic) Children() []Element {
+	return nil
 }
 
 type Cubic struct {
 }
 
+func (e *Cubic) Parent() []Element {
+	return nil
+}
+
+func (e *Cubic) Children() []Element {
+	return nil
+}
+
 type Lanes struct {
+}
+
+func (e *Lanes) Parent() []Element {
+	return nil
+}
+
+func (e *Lanes) Children() []Element {
+	return nil
 }
 
 type LaneOffset struct {
 }
 
+func (e *LaneOffset) Parent() []Element {
+	return nil
+}
+
+func (e *LaneOffset) Children() []Element {
+	return nil
+}
+
 type LaneSection struct {
+}
+
+func (e *LaneSection) Parent() []Element {
+	return nil
+}
+
+func (e *LaneSection) Children() []Element {
+	return nil
 }
 
 type Left struct {
 }
 
+func (e *Left) Parent() []Element {
+	return nil
+}
+
+func (e *Left) Children() []Element {
+	return nil
+}
+
 type Lane struct {
+}
+
+func (e *Lane) Parent() []Element {
+	return nil
+}
+
+func (e *Lane) Children() []Element {
+	return nil
 }
 
 type Border struct {
 }
 
+func (e *Border) Parent() []Element {
+	return nil
+}
+
+func (e *Border) Children() []Element {
+	return nil
+}
+
 type RoadMark struct {
+}
+
+func (e *RoadMark) Parent() []Element {
+	return nil
+}
+
+func (e *RoadMark) Children() []Element {
+	return nil
 }
 
 type Sway struct {
 }
 
+func (e *Sway) Parent() []Element {
+	return nil
+}
+
+func (e *Sway) Children() []Element {
+	return nil
+}
+
 type Explicit struct {
+}
+
+func (e *Explicit) Parent() []Element {
+	return nil
+}
+
+func (e *Explicit) Children() []Element {
+	return nil
 }
 
 type Material struct {
 }
 
+func (e *Material) Parent() []Element {
+	return nil
+}
+
+func (e *Material) Children() []Element {
+	return nil
+}
+
 type Access struct {
+}
+
+func (e *Access) Parent() []Element {
+	return nil
+}
+
+func (e *Access) Children() []Element {
+	return nil
 }
 
 type Restriction struct {
 }
 
+func (e *Restriction) Parent() []Element {
+	return nil
+}
+
+func (e *Restriction) Children() []Element {
+	return nil
+}
+
 type Height struct {
+}
+
+func (e *Height) Parent() []Element {
+	return nil
+}
+
+func (e *Height) Children() []Element {
+	return nil
 }
 
 type Rule struct {
 }
 
+func (e *Rule) Parent() []Element {
+	return nil
+}
+
+func (e *Rule) Children() []Element {
+	return nil
+}
+
 type Center struct {
+}
+
+func (e *Center) Parent() []Element {
+	return nil
+}
+
+func (e *Center) Children() []Element {
+	return nil
 }
 
 type Right struct {
 }
 
+func (e *Right) Parent() []Element {
+	return nil
+}
+
+func (e *Right) Children() []Element {
+	return nil
+}
+
 type Objects struct {
+}
+
+func (e *Objects) Parent() []Element {
+	return nil
+}
+
+func (e *Objects) Children() []Element {
+	return nil
 }
 
 type Object struct {
 }
 
+func (e *Object) Parent() []Element {
+	return nil
+}
+
+func (e *Object) Children() []Element {
+	return nil
+}
+
 type Repeat struct {
+}
+
+func (e *Repeat) Parent() []Element {
+	return nil
+}
+
+func (e *Repeat) Children() []Element {
+	return nil
 }
 
 type Outline struct {
 }
 
+func (e *Outline) Parent() []Element {
+	return nil
+}
+
+func (e *Outline) Children() []Element {
+	return nil
+}
+
 type CornerRoad struct {
+}
+
+func (e *CornerRoad) Parent() []Element {
+	return nil
+}
+
+func (e *CornerRoad) Children() []Element {
+	return nil
 }
 
 type CornerLocal struct {
 }
 
+func (e *CornerLocal) Parent() []Element {
+	return nil
+}
+
+func (e *CornerLocal) Children() []Element {
+	return nil
+}
+
 type Outlines struct {
+}
+
+func (e *Outlines) Parent() []Element {
+	return nil
+}
+
+func (e *Outlines) Children() []Element {
+	return nil
 }
 
 type Validity struct {
 }
 
+func (e *Validity) Parent() []Element {
+	return nil
+}
+
+func (e *Validity) Children() []Element {
+	return nil
+}
+
 type ParkingSpace struct {
+}
+
+func (e *ParkingSpace) Parent() []Element {
+	return nil
+}
+
+func (e *ParkingSpace) Children() []Element {
+	return nil
 }
 
 type Markings struct {
 }
 
+func (e *Markings) Parent() []Element {
+	return nil
+}
+
+func (e *Markings) Children() []Element {
+	return nil
+}
+
 type Marking struct {
+}
+
+func (e *Marking) Parent() []Element {
+	return nil
+}
+
+func (e *Marking) Children() []Element {
+	return nil
 }
 
 type CornerReference struct {
 }
 
+func (e *CornerReference) Parent() []Element {
+	return nil
+}
+
+func (e *CornerReference) Children() []Element {
+	return nil
+}
+
 type Borders struct {
+}
+
+func (e *Borders) Parent() []Element {
+	return nil
+}
+
+func (e *Borders) Children() []Element {
+	return nil
 }
 
 type Surface struct {
 }
 
+func (e *Surface) Parent() []Element {
+	return nil
+}
+
+func (e *Surface) Children() []Element {
+	return nil
+}
+
 type Skeleton struct {
+}
+
+func (e *Skeleton) Parent() []Element {
+	return nil
+}
+
+func (e *Skeleton) Children() []Element {
+	return nil
 }
 
 type Polyline struct {
 }
 
+func (e *Polyline) Parent() []Element {
+	return nil
+}
+
+func (e *Polyline) Children() []Element {
+	return nil
+}
+
 type VertexRoad struct {
+}
+
+func (e *VertexRoad) Parent() []Element {
+	return nil
+}
+
+func (e *VertexRoad) Children() []Element {
+	return nil
 }
 
 type VertexLocal struct {
 }
 
+func (e *VertexLocal) Parent() []Element {
+	return nil
+}
+
+func (e *VertexLocal) Children() []Element {
+	return nil
+}
+
 type ObjectReference struct {
+}
+
+func (e *ObjectReference) Parent() []Element {
+	return nil
+}
+
+func (e *ObjectReference) Children() []Element {
+	return nil
 }
 
 type Tunnel struct {
 }
 
+func (e *Tunnel) Parent() []Element {
+	return nil
+}
+
+func (e *Tunnel) Children() []Element {
+	return nil
+}
+
 type Bridge struct {
+}
+
+func (e *Bridge) Parent() []Element {
+	return nil
+}
+
+func (e *Bridge) Children() []Element {
+	return nil
 }
 
 type Signals struct {
 }
 
+func (e *Signals) Parent() []Element {
+	return nil
+}
+
+func (e *Signals) Children() []Element {
+	return nil
+}
+
 type Signal struct {
+}
+
+func (e *Signal) Parent() []Element {
+	return nil
+}
+
+func (e *Signal) Children() []Element {
+	return nil
 }
 
 type PositionInertial struct {
 }
 
+func (e *PositionInertial) Parent() []Element {
+	return nil
+}
+
+func (e *PositionInertial) Children() []Element {
+	return nil
+}
+
 type PositionRoad struct {
+}
+
+func (e *PositionRoad) Parent() []Element {
+	return nil
+}
+
+func (e *PositionRoad) Children() []Element {
+	return nil
 }
 
 type Dependency struct {
 }
 
+func (e *Dependency) Parent() []Element {
+	return nil
+}
+
+func (e *Dependency) Children() []Element {
+	return nil
+}
+
 type Reference struct {
+}
+
+func (e *Reference) Parent() []Element {
+	return nil
+}
+
+func (e *Reference) Children() []Element {
+	return nil
 }
 
 type SignalReference struct {
 }
 
+func (e *SignalReference) Parent() []Element {
+	return nil
+}
+
+func (e *SignalReference) Children() []Element {
+	return nil
+}
+
 type CRG struct {
+}
+
+func (e *CRG) Parent() []Element {
+	return nil
+}
+
+func (e *CRG) Children() []Element {
+	return nil
 }
 
 type Railroad struct {
 }
 
+func (e *Railroad) Parent() []Element {
+	return nil
+}
+
+func (e *Railroad) Children() []Element {
+	return nil
+}
+
 type Switch struct {
+}
+
+func (e *Switch) Parent() []Element {
+	return nil
+}
+
+func (e *Switch) Children() []Element {
+	return nil
 }
 
 type MainTrack struct {
 }
 
+func (e *MainTrack) Parent() []Element {
+	return nil
+}
+
+func (e *MainTrack) Children() []Element {
+	return nil
+}
+
 type SideTrack struct {
+}
+
+func (e *SideTrack) Parent() []Element {
+	return nil
+}
+
+func (e *SideTrack) Children() []Element {
+	return nil
 }
 
 type Partner struct {
 }
 
+func (e *Partner) Parent() []Element {
+	return nil
+}
+
+func (e *Partner) Children() []Element {
+	return nil
+}
+
 type Controller struct {
+}
+
+func (e *Controller) Parent() []Element {
+	return nil
+}
+
+func (e *Controller) Children() []Element {
+	return nil
 }
 
 type Control struct {
 }
 
+func (e *Control) Parent() []Element {
+	return nil
+}
+
+func (e *Control) Children() []Element {
+	return nil
+}
+
 type JunctiontypeCrossing struct {
+}
+
+func (e *JunctiontypeCrossing) Parent() []Element {
+	return nil
+}
+
+func (e *JunctiontypeCrossing) Children() []Element {
+	return nil
 }
 
 type RoadSection struct {
 }
 
+func (e *RoadSection) Parent() []Element {
+	return nil
+}
+
+func (e *RoadSection) Children() []Element {
+	return nil
+}
+
 type Priority struct {
+}
+
+func (e *Priority) Parent() []Element {
+	return nil
+}
+
+func (e *Priority) Children() []Element {
+	return nil
 }
 
 type JunctiontypeDefault struct {
 }
 
+func (e *JunctiontypeDefault) Parent() []Element {
+	return nil
+}
+
+func (e *JunctiontypeDefault) Children() []Element {
+	return nil
+}
+
 type Connection struct {
+}
+
+func (e *Connection) Parent() []Element {
+	return nil
+}
+
+func (e *Connection) Children() []Element {
+	return nil
 }
 
 type LaneLink struct {
 }
 
+func (e *LaneLink) Parent() []Element {
+	return nil
+}
+
+func (e *LaneLink) Children() []Element {
+	return nil
+}
+
 type CrossPath struct {
+}
+
+func (e *CrossPath) Parent() []Element {
+	return nil
+}
+
+func (e *CrossPath) Children() []Element {
+	return nil
 }
 
 type StartLaneLink struct {
 }
 
+func (e *StartLaneLink) Parent() []Element {
+	return nil
+}
+
+func (e *StartLaneLink) Children() []Element {
+	return nil
+}
+
 type EndLaneLink struct {
+}
+
+func (e *EndLaneLink) Parent() []Element {
+	return nil
+}
+
+func (e *EndLaneLink) Children() []Element {
+	return nil
 }
 
 type Boundary struct {
 }
 
+func (e *Boundary) Parent() []Element {
+	return nil
+}
+
+func (e *Boundary) Children() []Element {
+	return nil
+}
+
 type SegmenttypeJoint struct {
+}
+
+func (e *SegmenttypeJoint) Parent() []Element {
+	return nil
+}
+
+func (e *SegmenttypeJoint) Children() []Element {
+	return nil
 }
 
 type SegmenttypeLane struct {
 }
 
+func (e *SegmenttypeLane) Parent() []Element {
+	return nil
+}
+
+func (e *SegmenttypeLane) Children() []Element {
+	return nil
+}
+
 type ElevationGrid struct {
+}
+
+func (e *ElevationGrid) Parent() []Element {
+	return nil
+}
+
+func (e *ElevationGrid) Children() []Element {
+	return nil
 }
 
 type JunctiontypeDirect struct {
 }
 
+func (e *JunctiontypeDirect) Parent() []Element {
+	return nil
+}
+
+func (e *JunctiontypeDirect) Children() []Element {
+	return nil
+}
+
 type JunctiontypeVirtual struct {
+}
+
+func (e *JunctiontypeVirtual) Parent() []Element {
+	return nil
+}
+
+func (e *JunctiontypeVirtual) Children() []Element {
+	return nil
 }
 
 type ConnectiontypeDefault struct {
 }
 
+func (e *ConnectiontypeDefault) Parent() []Element {
+	return nil
+}
+
+func (e *ConnectiontypeDefault) Children() []Element {
+	return nil
+}
+
 type ConnectiontypeVirtual struct {
+}
+
+func (e *ConnectiontypeVirtual) Parent() []Element {
+	return nil
+}
+
+func (e *ConnectiontypeVirtual) Children() []Element {
+	return nil
 }
 
 type JunctionGroup struct {
 }
 
+func (e *JunctionGroup) Parent() []Element {
+	return nil
+}
+
+func (e *JunctionGroup) Children() []Element {
+	return nil
+}
+
 type JunctionReference struct {
+}
+
+func (e *JunctionReference) Parent() []Element {
+	return nil
+}
+
+func (e *JunctionReference) Children() []Element {
+	return nil
 }
 
 type Station struct {
 }
 
+func (e *Station) Parent() []Element {
+	return nil
+}
+
+func (e *Station) Children() []Element {
+	return nil
+}
+
 type Platform struct {
 }
 
+func (e *Platform) Parent() []Element {
+	return nil
+}
+
+func (e *Platform) Children() []Element {
+	return nil
+}
+
 type Segment struct {
+}
+
+func (e *Segment) Parent() []Element {
+	return nil
+}
+
+func (e *Segment) Children() []Element {
+	return nil
 }
